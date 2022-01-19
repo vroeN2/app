8eb84398f86b8abaf8cf5c563b5e461a83a235aa
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2022-01-19 10:21:21 +0100

    feat: add basic page structure
    - install required dependencies
    - create dashboard components
    - use dummy data to check layout

---
+++ src/features/dashboard/Topbar.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const TopbarWrapper = styled.div``;
+
+interface Props {}
+
+const Topbar = (props: Props) => {
+  return (
+    <TopbarWrapper>
+      <h1>TOPBAR</h1>
+    </TopbarWrapper>
+  );
+};
+
+export default Topbar;
